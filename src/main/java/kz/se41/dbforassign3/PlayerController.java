0659d754e8b742c83a0750fb51f59faf7b7da458
beymz <beymzhan777@gmail.com>
2026-02-11 21:14:25 +0500

    5st create entities

--- src/main/java/kz/se41/dbforassign3/PlayerController.java
+++ src/main/java/kz/se41/dbforassign3/PlayerController.java
@@ -3,20 +3,34 @@ package kz.se41.dbforassign3;
 import kz.se41.dbforassign3.entities.Player;
 import kz.se41.dbforassign3.entities_service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/player")
 public class PlayerController {
-
-
     @Autowired
     private PlayerService playerService;
 
-    @GetMapping("/player")
+    @GetMapping
     public List<Player> getAllPlayers() {
         return playerService.getAllPlayers();
+    }
+
+    @PostMapping
+    public Player create(@RequestBody Player player) {
+        return playerService.create(player);
+    }
+
+    @PutMapping("/{id}")
+    public Player update(@PathVariable Long id,
+                         @RequestBody Player player) {
+        return playerService.update(id, player);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        playerService.delete(id);
     }
 }
--- src/main/java/kz/se41/dbforassign3/entities_service/PlayerService.java
+++ src/main/java/kz/se41/dbforassign3/entities_service/PlayerService.java
@@ -10,9 +10,34 @@ import java.util.List;
 
 @Service
 public class PlayerService {
+
+    public Player getById(Long id) {
+        return playerRep.findById(id)
+                .orElseThrow(() -> new RuntimeException("Player not found"));
+    }
+
     @Autowired
     private PlayerRep playerRep;
     public List<Player> getAllPlayers(){
         return playerRep.findAll();
+    }
+    public Player create(Player player) {
+        player.setId(null);
+        return playerRep.save(player);
+    }
+    public Player update(Long id, Player updated) {
+        Player player = getById(id);
+
+        player.setName(updated.getName());
+        player.setHeight(updated.getHeight());
+        player.setWeight(updated.getWeight());
+        player.setSkill(updated.getSkill());
+        return playerRep.save(player);
+    }
+    public void delete(Long id) {
+        if (!playerRep.existsById(id)) {
+            throw new RuntimeException("Player not found");
+        }
+        playerRep.deleteById(id);
     }
 }
